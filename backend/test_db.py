c1e459496cfaaade65ad99e7753f4deae6bcde56
Ashankavinda277 <bandaralamak.23@uom.lk>
2026-08-27 20:12:15 +0530

    chore: setup database and alembic migrations

---
+++ backend/test_db.py
@@ -0,0 +1,7 @@
+from app.db.database import engine
+from sqlalchemy import text
+
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT version();"))
+
+    print(result.fetchone())
